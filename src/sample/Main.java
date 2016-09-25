f03e5d946a9e905bb6d2b1287d46c41099dc65d3
viktormushak <viktor.mushak@gmail.com>
2016-09-25 19:23:21 +0300

    chang title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Bankomat");
         primaryStage.setScene(new Scene(root, 640, 400));
         primaryStage.show();
     }
