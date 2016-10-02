38c4680b79918a279f74fa1fba582201243961e1
inlaing <inlaing@mail.ru>
2016-10-02 20:15:42 +0300

    add logo

--- src/server/Main.java
+++ src/server/Main.java
@@ -6,6 +6,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -14,6 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.getIcons().add(new Image("file:logo.png"));
         primaryStage.setTitle("Server Banking");
         primaryStage.setScene(new Scene(root, 640, 400));
         primaryStage.show();
